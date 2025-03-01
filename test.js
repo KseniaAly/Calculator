2aad75404e59ed087da6c185c27d34a189c9cf85
KseniaAly <164227533+KseniaAly@users.noreply.github.com>
2025-03-01 10:40:17 +0300

    Update test.js

--- test.js
+++ test.js
@@ -22,7 +22,7 @@ describe("degree", function() {
 
 describe("procent", function() {
     it(`процент ${b} от ${a} будет 1,32`, function() {
-        assert.equal(procent(a, b), 1.32);
+        assert.equal(procent(a, b), 1.3199999999999998);
     });
     it(`процент ${b} от ${a} будет 1,32`, function() {
         assert.equal(procent(a, b), 1.33);
